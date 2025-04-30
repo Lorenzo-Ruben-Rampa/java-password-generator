cdbd73d40f3f4b84779cb1084bad563dd6d6b3b9
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-04-30 13:25:35 +0200

    correzione consegna

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,27 +1,52 @@
 package org.lessons.java.security;
 
-import java.sql.Date;
 import java.util.Scanner;
 
 public class PasswordGenerator {
     public static void main(String[] args) {
+
+        String nome;
+        String cognome;
+        String colore;
+        // Date dataNascita = Date.valueOf("1993-05-15");
+        int birthDay;
+        int birthMonth;
+        int birthYear;
+
+        // Recupero i dati dall'utente
         Scanner input = new Scanner(System.in);
         
         System.out.println("Inserisci il tuo nome: ");
-        String nome = input.nextLine();
+        nome = input.nextLine();
         
         System.out.println("Inserisci il tuo cognome: ");
-        String cognome = input.nextLine();
+        cognome = input.nextLine();
         
         System.out.println("Inserisci il tuo colore preferito: ");
-        String colore = input.nextLine();
+        colore = input.nextLine();
+                      
+        System.out.println("Inserisci il numero del giorno della tua data di nascita: ");
+        birthDay = input.nextInt();
+
+        System.out.println("Inserisci il numero del mese della tua data di nascita: ");
+        birthMonth = input.nextInt();
+
+        System.out.println("Inserisci l'anno della tua data di nascita: ");
+        birthYear = input.nextInt();
+
+        int dataNascita = birthYear + birthMonth + birthDay;
+
         
-        System.out.println("Inserisci la tua data di nascita (yyyy-mm-dd): ");
-        Date dataNascita = Date.valueOf(input.nextLine());
+        String password = nome + "-" + cognome + "-" + colore.toLowerCase() + "-" + dataNascita;
+
         
-        String password = nome + "-" + cognome + "-" + colore.toLowerCase() + "-" + dataNascita.toString().substring(0, 4);
+        System.out.println("La tua password generata e': " + password);
+
+        // System.out.println("Inserisci la tua data di nascita (yyyy-mm-dd): ");
+        // Date dataNascita = Date.valueOf(input.nextLine());
         
-        System.out.println("La tua password generata " + '\u00E9' + ": " + password);
+        // String password = nome + "-" + cognome + "-" + colore.toLowerCase() + "-" + dataNascita.toString().substring(0, 4);
+
 
     }
 }
